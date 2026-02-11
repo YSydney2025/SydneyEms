151928b4eeb7479b96cfb5b8f8394c3060a7bdea
YSydney2025 <eyondjin8@gmai.com>
2026-02-11 20:03:17 +0100

    "first lessons"

--- Day1.py
+++ Day1.py
@@ -1,1 +1,5 @@
-num = 60
+nom = "Alex"
+objectif = "devenir CTO"
+age = 30
+
+print("Je m'appelle", nom,"et j'ai",age,".\nMon seul objectif est de", objectif, "pour revolutioner le monde de la tech")
